852019f7ec61465fbfbcf0c466bda630362730f6
Daria Natalina <natalyina.darina@yandex.ru>
2021-01-06 04:20:26 +0300

    v6.0

--- main.js
+++ main.js
@@ -7,24 +7,55 @@ $(document).ready(function () {
         alert('Ура, ты нажал на картинку!');
     });
     //.toggleClass() 
-    $('h1').dblclick(function(){
+    $('h1').dblclick(function () {
         $(this).toggleClass('blue')
     });
     //.hide() 
-    $('#hide').hide();
+    $('button.hide').click(function(){
+        $('img.hide').hide();
+    });
+    //.show()
+    $('button.show').click(function(){
+        $('img.hide').show();
+    });
     //.change() 
-    $('input').change(function(){
-    //.text() 
-        $('#userName').text(', '+$(this).val()+'!');
+    $('input').change(function () {
+        //.text() 
+        $('#userName').text(', ' + $(this).val() + '!');
     });
     //.keypress()
-    $('input').keypress(function(){
-        $('#userName').text(', '+$(this).val()+'!');
+    $('input.imya').keypress(function () {
+        $('#userName').text(', ' + $(this).val() + '!');
     });
     //.keyup()
-    $('input').keyup(function(){
-        $('#userName').text(', '+$(this).val()+'!');
+    $('input.imya').keyup(function () {
+        $('#userName').text(', ' + $(this).val() + '!');
     });
     //.addClass()
     $('div').last().addClass('blue');
+    //.html()
+    $('p.html').click(function () {
+        var html = $(this).html();
+        $(this).text(html);
+    });
+    //.trigger()
+    $('button.trig').click(function () {
+        update($("span.trig"));
+    });
+
+    function update(j) {
+        var n = parseInt(j.text(), 10);
+        j.text(n + 1);
+    };
+    //.prop()
+    //.attr()
+    //.find()
+    //.filter()
+    
+    //.wrap()
+    //.prepend()
+    //.append()
+
+
+
 });
